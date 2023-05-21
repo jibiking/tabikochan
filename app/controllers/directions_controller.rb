9d407b07f4372691764fa87ff8d783b1298f88c6
jibiking <daiki.jibiki@gmail.com>
2023-05-21 21:22:49 +0900

    入力値をマップページ下部に表示OK

--- app/controllers/directions_controller.rb
+++ app/controllers/directions_controller.rb
@@ -9,5 +9,13 @@ class DirectionsController < ApplicationController
     redirect_to result_path(start_location: start_location, end_location: end_location, waypoint: waypoints)
   end
 
-  def result; end
+  def result
+    start_location = params[:start_location]
+    end_location = params[:end_location]
+    waypoint = params[:waypoint]
+
+    @start_location = start_location
+    @end_location = end_location
+    @waypoint = waypoint.split('/')
+  end
 end
